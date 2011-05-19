22f919fd7719cd7fb137e56465373aae13fe20a1
Jag Srawan <jag@mobbazaar.com>
2011-05-19 13:18:31 -0700

    Cleaned up results.

--- src/homeworks/groupplanet/mrbigplanet/src/treepredict.py
+++ src/homeworks/groupplanet/mrbigplanet/src/treepredict.py
@@ -3,6 +3,7 @@
 
 from optparse import OptionParser
 import json
+import math
 
 
 my_data=[['slashdot','USA','yes',18,'None'],
@@ -22,6 +23,8 @@ my_data=[['slashdot','USA','yes',18,'None'],
         ['google','UK','yes',18,'Basic'],
         ['kiwitobes','France','yes',19,'Basic']]
 
+# This class is just to hold the decision nodes, which decide the col, splitValue, results=labels for leaf nodes, and true/false branch is the number of nodes for each 
+# branch label.
 class decisionnode:
     def __init__(self,col=-1,value=None,results=None,tb=None,fb=None):
         self.col=col
@@ -35,7 +38,7 @@ def divideset(rows,column,value):
         # the first group (true) or the second group (false)
         split_function=None
         if isinstance(value,int) or isinstance(value,float):
-            split_function=lambda row:row[column]>=value
+            split_function=lambda row:row[column]>=value  #note larger than equal to for left node.
         else:
             split_function=lambda row:row[column]==value
         # Divide the rows into two sets and return them
@@ -45,6 +48,9 @@ def divideset(rows,column,value):
     
 # Create counts of possible results (the last column of
 # each row is the result)
+# Takes the class label, and does a count(label)
+# Return is a list of counts for each output label.
+# i.e. label1=10, label2=10
 def uniquecounts(rows):
     results={}
     for row in rows:
@@ -57,16 +63,16 @@ def uniquecounts(rows):
     
 # Probability that a randomly placed item will
 # be in the wrong category
+# This code is super wierd, instead of 1- sum-square impurities
+# It does dot product instead, but the numbers come out the same.
 def giniimpurity(rows):
     total=len(rows)
     counts=uniquecounts(rows)
     imp=0
     for k1 in counts:
-        p1=float(counts[k1])/total
-        for k2 in counts:
-            if k1==k2: continue
-            p2=float(counts[k2])/total
-            imp+=p1*p2
+        p= math.pow((float(counts[k1])/total),2)
+        imp+=p
+    imp = 1- imp
     return imp
 
 # Entropy is the sum of p(x)log(p(x)) across all
@@ -82,17 +88,34 @@ def entropy(rows):
         ent=ent-p*log2(p)
     return ent
 
+# Information gain between parent and children
+# current score, is parent score.
+#
 
+def informationGain (parentScore, parentRows, childRows1, childRows2, scoref=entropy):
+
+    p=float(len(childRows1))/len(parentRows)
+    gain=parentScore-p*scoref(childRows1)-(1-p)*scoref(childRows2)
+    return gain;
+
+# buildtree - main function for tree building
+# 
+#
+#
+#
 def buildtree(rows,scoref=entropy):
     
     
     if len(rows)==0: return decisionnode( )
     
     #Jag, increase the number of nodes allowed for pure node
-    if len(rows) < 100: return decisionnode(results=uniquecounts(rows) )
+    if len(rows) < 10: return decisionnode(results=uniquecounts(rows) )
     
     current_score=scoref(rows)
+    
     # Set up some variables to track the best criteria
+    # We just loop through each posibility to find the best split.
+    # Then we takes those sets and call recurively down to the children nodes.
     best_gain=0.0
     best_criteria=None
     best_sets=None
@@ -105,11 +128,12 @@ def buildtree(rows,scoref=entropy):
             column_values[row[col]]=1
         # Now try dividing the rows up for each value
         # in this column
+        # This is a bit inefficient, it could sort values and find midpoint rather than this way.
         for value in column_values.keys( ):
             (set1,set2)=divideset(rows,col,value)
+        
         # Information gain
-            p=float(len(set1))/len(rows)
-            gain=current_score-p*scoref(set1)-(1-p)*scoref(set2)
+            gain=informationGain (current_score, rows, set1, set2)
             if gain>best_gain and len(set1)>0 and len(set2)>0:
                 best_gain=gain
                 best_criteria=(col,value)
@@ -118,12 +142,13 @@ def buildtree(rows,scoref=entropy):
     if best_gain>0:
         trueBranch=buildtree(best_sets[0])
         falseBranch=buildtree(best_sets[1])
-        return decisionnode(col=best_criteria[0],value=best_criteria[1],tb=trueBranch,fb=falseBranch)
+        return decisionnode( col=best_criteria[0],value=best_criteria[1],tb=trueBranch,fb=falseBranch )
     else:
-        return decisionnode(results=uniquecounts(rows))
+        return decisionnode( results=uniquecounts(rows) )
 
 def printtree(tree,indent=''):
-    # Is this a leaf node?
+    
+    # Is this a leaf node? Then print out the count of each of the class labels.
     if tree.results!=None:
         print str(tree.results)
     else:
@@ -161,7 +186,14 @@ def main() :
         fileIn = open(filePath)
         paramJson = fileIn.read()
         fileIn.close()
-        theData = json.loads( paramJson )
+        loadRows = json.loads( paramJson )
+        
+        #Get rid of first two columns
+        theData = []
+        for row in loadRows: 
+            rowCol = row[ 2 : (len(row)) ]
+            theData.append(rowCol)
+        
     else :
         theData = my_data
 
