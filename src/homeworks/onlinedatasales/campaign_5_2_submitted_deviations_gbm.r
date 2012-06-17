431e98fbaf95c1b74e4f077c260d7c3df003e652
Jag Srawan <jsrawan@gmail.com>
2012-06-16 21:52:24 -0700

    Modified but worse results.

--- src/homeworks/onlinedatasales/campaign_5_2_submitted_deviations_gbm.r
+++ src/homeworks/onlinedatasales/campaign_5_2_submitted_deviations_gbm.r
@@ -60,65 +60,77 @@ computeRMSLE <- function(Ysimulated, Yreal) {
 }
 
 
-#setup data
+#1. setup data
 #setwd("/Users/Ivanko/Machine Learning/R/Kaggle/productsales")
-data <- read.table(file="TrainingDataset.csv",header=TRUE, sep=",")
+traindata <- read.table(file="TrainingDataset.csv",header=TRUE, sep=",")
 testData <- read.table(file="TestDataset.csv",header=TRUE, sep=",")
 
-Xtrain <- data[,13:29]
+Xtrain <- traindata[,13:29]
 Xtest <- testData[,2:18]
-#add the difference between two dates as another variable
-daysbetweencampaigns <- as.numeric(data[,14]-data[,19])
-Xtrain <- cbind(Xtrain, daysbetweencampaigns)
-
-daysbetweencampaigns <- as.numeric(testData[,3]-testData[,8])
-Xtest <- cbind(Xtest, daysbetweencampaigns)
-
-YMonthlySales <- as.matrix(data[,1:12])
-YMonthlySales <- log(YMonthlySales)
-YMonthlySales[is.na(YMonthlySales)] <- 0.0
-
-YTotalSales <- as.numeric(data[,1])
-#Y - labels as sum of all month sales
-for(i in 1:nrow(data)){
-  YTotalSales[i] <- log(sum(data[i,1:12],na.rm=TRUE))
-}
-YTotalSales[is.na(YTotalSales)] <- 0.0
 
 #cleanup data - factor variables are still problematic on prediction
-Xtrain <- cleanInputDataForGBM(Xtrain)
-Xtest <- cleanInputDataForGBM(Xtest)
+XtrainClean <- cleanInputDataForGBM(Xtrain)
+XtestClean <- cleanInputDataForGBM(Xtest)
 ntestRows <- nrow(Xtest)
-Ytest <- matrix(nrow = ntestRows, ncol = 12)
+ntrainRows <- nrow(Xtrain)
 
-numberOfRows <- nrow(Xtrain)
+#2. add the difference between two dates as another variable.  
+# Date
+# 
+#
+daysbetweencampaignsTrain <- as.numeric(Xtrain[,2]-Xtrain[,7])
+daysbetweencampaignsTrain[is.na(daysbetweencampaignsTrain)] <- 0.0
+Xtrain <- cbind(Xtrain, daysbetweencampaignsTrain)
+
+#
+#
+daysbetweencampaignsTest <- as.numeric(Xtest[,2]-testData[,7])
+daysbetweencampaignsTest[is.na(daysbetweencampaignsTest)] <- 0.0
+Xtest <- cbind(Xtest, daysbetweencampaignsTest)
+
+
+YMonthlySalesTrain <- log(as.matrix(traindata[,1:12]))
+YMonthlySalesTrain[is.na(YMonthlySalesTrain)] <- 0.0
+
+
+# Note I made a fundamental switch here
+# Any data that was N/A was being cleaned in the total
+# I do it ahead of time so we always get a sum
+YTotalSalesTrain <- matrix(nrow = ntrainRows, ncol = 1)
+#Y - labels as sum of all month sales
+for(i in 1:nrow(traindata)){
+	YTotalSalesTrain[i] <- log(sum(traindata[i,1:12],na.rm=TRUE))
+}
+
+
+
 frac <- rep(0.0, 12)
+sumAll <- sum(exp(YTotalSalesTrain))
 
-#calculate sum of all sales for the current fold
-sumAll <- sum(exp(YTotalSales))
-
-#find "average" time series behavior
+# find "average" time series behavior.  This is modelling the average time series
+# 
 #1.  volume weighted  
 #frac is total by month / grand total
 for(i in 1:12){
-  frac [i] <- sum(exp(YMonthlySales[,i]),na.rm=TRUE)/sumAll
+  frac [i] <- sum(exp(YMonthlySalesTrain[,i]),na.rm=TRUE)/sumAll
 }
 
 #some checks
 #see if frac sums to 1
-sum(frac)
 #plot to see if it's reasonable
+sum(frac)
 plot(frac)
 
-#estimate and predict total sales
-gdata <- cbind(YTotalSales,Xtrain)
+# Model to predice total sales
+# estimate and predict total sales as function of x
+gdata <- cbind(YTotalSalesTrain, XtrainClean)
 ntrees <- 4000
 depth <- 5
 minObs <- 10
 shrink <- 0.001
 folds <- 10
 
-mo1gbm <- gbm(YTotalSales~. ,data=gdata,
+mo1gbm <- gbm(YTotalSalesTrain~. ,data=gdata,
               distribution = "gaussian",
               n.trees = ntrees,
               shrinkage = shrink,
@@ -130,40 +142,61 @@ gbm.perf(mo1gbm,method="cv")
 mo1gbm$cv.error
 
 
-YPredictedAnnualTest <- predict.gbm(mo1gbm, newdata=Xtest, n.trees = ntrees)
-YPredictedAnnualTrain <- predict.gbm(mo1gbm, newdata=Xtrain, n.trees = ntrees)
+YPredictedAnnualTest <- predict.gbm(mo1gbm, newdata=XtestClean, n.trees = ntrees)
+YPredictedAnnualTrain <- predict.gbm(mo1gbm, newdata=XtrainClean, n.trees = ntrees)
 
 # Currently we can predict the output to 0.54.  This is pretty weak (not using all inputs!)
-computeRMSLE( exp(YPredictedAnnualTrain), exp(YTotalSales) )
+rmlseOfTotalSales = computeRMSLE( exp(YPredictedAnnualTrain), exp(YTotalSalesTrain) )
+rmlseOfTotalSales
+
+Yhattest <- matrix(nrow = ntestRows, ncol = 12)
+Yhattrain <- matrix(nrow = ntrainRows, ncol = 12)
 
 #now estimate and predict deviations from the "average" monthly portions of the total
 for( i in 1:12 ) {
   #get the deviation from the average for a given month's sales
-  YDeviationThisMonth <- exp(YMonthlySales[,i])/exp(YTotalSales) - frac[i]
+  YDeviationThisMonth <- exp(YMonthlySalesTrain[,i])/exp(YTotalSalesTrain) - frac[i]
   YDeviationThisMonth[is.na(YDeviationThisMonth)] <- 0.0
   
-  gdata <- cbind(YDeviationThisMonth, Xtrain)
+  gdata <- cbind(YDeviationThisMonth, XtrainClean)
+  
+  cat ("*****************************************************************", '\n' )
+  cat ("********************Column", i,"*************************",'\n' )
+  cat ("*****************************************************************",'\n' )
   
   #fit the model
-  mo2gbm <- gbm(YDeviationThisMonth~. ,
-                data=gdata,
+  mo2gbm <- gbm.fit(x=Xtrain, y=YDeviationThisMonth,
+                #data=gdata,
                 distribution = "laplace",
                 n.trees = ntrees,
                 shrinkage = shrink,
-                cv.folds = folds)
+                #cv.folds = folds
+  			  )
   
   #apply the model
-  monthlySalesDeviationFromAverage <- predict.gbm(mo2gbm, newdata=Xtest, n.trees = ntrees)
+  monthlySalesDeviationFromAverage <- predict.gbm(mo2gbm, newdata=XtestClean, n.trees = ntrees)
+  monthlySalesDeviationFromAverageTrain <- predict.gbm(mo2gbm, newdata=XtrainClean, n.trees = ntrees)
+  
    
   #save monthly sales prediction
-  Ytest[,i] <- monthlySalesDeviationFromAverage * exp(YPredictedAnnualTest) + frac[i] * exp(YPredictedAnnualTest)
-    gc()
+  Yhattest[,i] <- monthlySalesDeviationFromAverage * exp(YPredictedAnnualTest) + frac[i] * exp(YPredictedAnnualTest)
+  Yhattrain[,i] <- monthlySalesDeviationFromAverageTrain * exp(YPredictedAnnualTrain) + frac[i] * exp(YPredictedAnnualTrain)
+  gc()
+  
 }
 
 #cvError <- sum(rmsles)/10
+
+
+YhattrainRMLSE <- Yhattrain
+YtrainRMLSE <- as.matrix(traindata[,1:12])
+YtrainRMLSE[is.na(YtrainRMLSE)] <- 0.0
+rmsle <- computeRMSLE(YhattrainRMLSE, YtrainRMLSE)
+rmsle
+
 indices = seq(1,ntestRows,1)
-Ytest <- cbind(indices,Ytest)
-write.csv(Ytest, "campaign_5_deviations_gbm.csv", row.names=FALSE)
+Ytest <- cbind(indices,Yhattest)
+write.csv(Yhattest, "campaign_5_deviations_gbm.csv", row.names=FALSE)
 
 
 
